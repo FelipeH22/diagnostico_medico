381148f56fe3e85d11175bb26c202d7a3920f6f7
FelipeH22 <juan22herrera01@gmail.com>
2020-08-17 17:55:51 -0500

    Se añaden pruebas en el prolog

--- diagnostico.py
+++ diagnostico.py
@@ -5,4 +5,8 @@ print('Bienvenidos')
 print('Segun la respuestas dadas tendra su resultado:')
 for result in prolog.query("enfermedades(Enfermedad)"):
 	resultado = result['Enfermedad']
-print('De acuerdo con sus respuestas,usted padece de: '+resultado)
+if resultado!="En mi base de conocimientos no figura ninguna enfermedad para la serie de sintomas descritos.":
+	print('De acuerdo con sus respuestas,usted puede padecer de: '+resultado)
+else:
+	print(resultado)
+print('Tenga presente que este diagnóstico no equivale a un diagnóstico profesional. Por tanto, se recomienda acudir al médico en caso de presentar complicaciones')
