554897589df4974f82b84959ba4b6cc840a8c315
FelipeH22 <juan22herrera01@gmail.com>
2020-08-26 16:34:22 -0500

    Cambios

--- diagnostico_django/diagnostico_django/urls.py
+++ diagnostico_django/diagnostico_django/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from diagnostico_django.diagnostico_django.vistas import muestra_pagina
+from diagnostico_django.vistas import muestra_pagina
 
 urlpatterns = [
     path('admin/', admin.site.urls),
