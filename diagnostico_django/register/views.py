22e93d14d2253432f5b7eaf32d494c5874279617
Cyara <cmyarap@correo.uditrital.edu.co>
2020-08-27 11:45:09 -0500

    Sistema de login

--- diagnostico_django/register/views.py
+++ diagnostico_django/register/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def registro(request):
     return render(request, "register/registro.html")
+
+
+def login_view(request):
+    """Login view."""
+    if request.method == 'POST':
+        correo = request.POST['correo']
+        user = authenticate(request, correo=correo)
+        if user:
+            login(request, user)
+            return redirect('diagnostico')
+        else:
+            return render(request, 'users/login.html', {'error': 'Invalid username and password'})
+    return render(request, 'register/login.html')
